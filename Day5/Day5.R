55bb72b4bdd59e78d6d6a7a73f8bcb1027f7e53a
Shoaib Ali Ajaib <ajxa@pm.me>
2022-12-05 08:52:58 +0000

    cleaned up day5 code

--- Day5/Day5.R
+++ Day5/Day5.R
@@ -1,39 +1,33 @@
-instructions <- readLines("Day5/input.txt")
+`%>%` <- magrittr::`%>%`
+input <- readLines("Day5/input.txt")
 
-instructions <- instructions[11:length(instructions)] |> strsplit(" ") |> 
-    lapply(\(x) setNames(as.numeric(c(x[[2]], x[[4]], x[[6]])),
-                         c("amount", "from", "to")))
-
-creates <- list('1'="RCH",
-                '2'="FSLHJB",
-                '3'="QTJHDMR",
-                '4'="JBZHRGS",
-                '5'="BCDTZFPR",
-                '6'="GCHT",
-                '7'="LWPBZVNS",
-                '8'="CGQJR",
-                '9'="SFPHRTDL")
-
-creates <- lapply(creates, \(x) quanteda::tokenize_character(x)) |> unlist(recursive = F)
+creates <- input[1:8] %>%  gsub("\\s{4}", "[!]", x = .) %>%  
+    gsub("\\[|\\]|\\s+", "", x = .) %>%
+    quanteda::tokenize_character() %>% 
+    purrr::map(~ifelse(.x == "!", NA, .x)) %>% as.data.frame() %>% 
+    split(seq(nrow(.))) %>% purrr::map(~as.character(na.omit(unlist(.x, use.names = F))))
+  
+instructions <- grep("^move", input, value = T) %>% strsplit(" ") %>% 
+    purrr::map(~ setNames(as.numeric(c(.x[[2]], .x[[4]], .x[[6]])),
+                          c("amount", "from", "to")))
 
 rearranged <- creates
 
 for (i in 1:length(instructions)) {
     
-    set = instructions[[i]]
-    
+    set <- instructions[[i]]
     
     if(set[["amount"]] > 1){
         
-        # move_quantity = set[["amount"]]:1 # part 1
-        move_quantity = 1:set[["amount"]] # part 2
+        # move_quantity <- set[["amount"]]:1 # part 1
+        move_quantity <- 1:set[["amount"]] # part 2
         
-        }else move_quantity = set[["amount"]]
+        }else move_quantity <- set[["amount"]]
     
-    rearranged[[set["to"]]] = c(rearranged[[set[["from"]]]][move_quantity], 
-                                rearranged[[set["to"]]])
+    rearranged[[set["to"]]] <- c(rearranged[[set[["from"]]]][move_quantity], 
+                                 rearranged[[set["to"]]])
     
-    rearranged[[set["from"]]]  = rearranged[[set["from"]]][-move_quantity]
+    rearranged[[set["from"]]] <- rearranged[[set["from"]]][-move_quantity]
 }
 
-sapply(rearranged, \(x) x[[1]]) |> paste0(collapse = "")
+rearranged %>% purrr::map_chr(~purrr::pluck(.x, 1)) %>% paste0(collapse = "")
