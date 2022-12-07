4aceff6ea0911b81de3f22571d56b29f9979deba
Shoaib Ali Ajaib <ajxa@pm.me>
2022-12-07 13:42:09 +0000

    added day7 solution

---
+++ Day7/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from collections import defaultdict
+
+input = map(str.split, open('input.txt').read().splitlines())
+
+path = []
+dirs = defaultdict(int)
+
+
+for line in input:
+    if line[0] == "$":
+        if line[1] == "cd":
+            if line[2] == "..":
+                path.pop()
+                # print(path)
+            else:
+                path.append(line[2])
+                # print(path)
+    elif line[0] != "dir":
+        for dir in range(len(path)):
+            dirs[tuple(path[: dir + 1])] += int(line[0])
+
+print(sum(size for size in dirs.values() if size <= 100000))
+
+required = 30000000 - (70000000 - dirs[('/',)])
+
+print(min(size for size in dirs.values() if size >= required))
