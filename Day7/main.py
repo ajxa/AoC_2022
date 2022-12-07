d1210b9f863e0123ec51e639a457552d9a2991f2
Shoaib Ali Ajaib <ajxa@pm.me>
2022-12-07 13:45:26 +0000

    Had to go with python for today - R was not the best tool for this task

--- Day7/main.py
+++ Day7/main.py
@@ -6,8 +6,8 @@ input = map(str.split, open('input.txt').read().splitlines())
 path = []
 dirs = defaultdict(int)
 
-
 for line in input:
+    
     if line[0] == "$":
         if line[1] == "cd":
             if line[2] == "..":
@@ -16,12 +16,14 @@ for line in input:
             else:
                 path.append(line[2])
                 # print(path)
+    
     elif line[0] != "dir":
         for dir in range(len(path)):
             dirs[tuple(path[: dir + 1])] += int(line[0])
-
+# Part 1
 print(sum(size for size in dirs.values() if size <= 100000))
 
+# part 2
 required = 30000000 - (70000000 - dirs[('/',)])
 
 print(min(size for size in dirs.values() if size >= required))
